e6f1fbbce51551eea9d17dd08b0b10abe6f86881
na-ka-mu-ra- <ryolailai@gmail.com>
2019-07-02 10:21:19 +0900

    refs #63421
    Branchが作られないといつ変更があったのか不明なので
    とりあえずCommit 内容に日付を付与するように暫定変更

--- scripts/create_pull_request_if_needed.sh
+++ scripts/create_pull_request_if_needed.sh
@@ -5,11 +5,10 @@ if [ "$?" -eq 0 ] ; then
   git config --global user.email eole.production@gmail.com
   git config --global user.name 'CircleCI'
   git add docs
-  git commit -a -m 'Update data'
+  git commit -a -m "Update data $BRANCH"
   #git branch -M $BRANCH
   #git push origin $BRANCH
   git push origin master
-  git branch -M $BRANCH
   #bundle exec ruby scripts/create_pull_request.rb
 else
   echo x-ken-all.csv not changed
